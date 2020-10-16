4f1319d401d82f8b1adad4a85fda89ae28a2447b
David <David.Kristensson@hotmail.com>
2020-10-16 17:20:14 +0200

    Added .gitignore file and removed compiled files. Implemented disco lightshow.

--- lab_01_baseline/main.c
+++ lab_01_baseline/main.c
@@ -3,13 +3,25 @@
 #include "serial.h"
 
 int main() {
-    DDRB |= (1 << PB0);
+    DDRB |= (1 << PB1);
+    DDRB |= (1 << PB2);
+    DDRB |= (1 << PB3);
 
     while (1) {
-        PORTB |= (1 << PB0);
-        _delay_ms(500);
-        PORTB &= ~(1 << PB0);
-        _delay_ms(500);
+        PORTB |= (1 << PB1);
+        _delay_ms(50);
+        PORTB &= ~(1 << PB1);
+        _delay_ms(50);
+
+        PORTB |= (1 << PB2);
+        _delay_ms(50);
+        PORTB &= ~(1 << PB2);
+        _delay_ms(50);
+
+        PORTB |= (1 << PB3);
+        _delay_ms(50);
+        PORTB &= ~(1 << PB3);
+        _delay_ms(50);
     }
 
     return 0;
